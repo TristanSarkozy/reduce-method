2c3387ab4c359af19c6db5c97e48a614c613f3de
Suciu Dan <suciu_t_dan@yahoo.com>
2024-02-12 14:27:18 +0000

    Add reduce method to group our team members by profession and find the total experience for each profession

--- reduce.js
+++ reduce.js
@@ -37,5 +37,17 @@ const teamMembers = [
 let totalExperience = teamMembers.reduce((acc, curr) => acc + curr.yrsExperience, 0);
 console.log(totalExperience);
 
-// Grouping by a property, and totaling it too
 
+// Grouping by a property, and totaling it too
+// {Developer: 12, Designer: 4} <-- this is what we want!
+let experienceByProfession = teamMembers.reduce((acc, curr) => {
+let key = curr.profession;
+if (!acc[key]) {
+  acc[key] = curr.yrsExperience;
+} else {
+  acc[key] += curr.yrsExperience;
+}
+return acc;
+}, {});
+
+console.log(experienceByProfession);
