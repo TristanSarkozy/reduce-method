04d330450ac2677e57018c1b998d049447d118ff
Suciu Dan <suciu_t_dan@yahoo.com>
2024-02-12 14:18:47 +0000

    Add the total experience as a whole

--- reduce.js
+++ reduce.js
@@ -7,16 +7,8 @@
 
 // Summing an array of numbers:
 const nums = [0, 1, 2, 3, 4];
-let sum = nums.reduce((acc, curr) => {
-  console.log(
-    "Accumulator:", acc,
-    "Current Value:", curr,
-    "Total:", acc + curr
-  );
-  return acc + curr;
-}, 0);
+let sum = nums.reduce((acc, curr) => acc + curr, 0);
 console.log(sum);
-
 
 const teamMembers = [
   {
@@ -42,6 +34,8 @@ const teamMembers = [
 ];
 
 // Totaling a specific object property
-
+let totalExperience = teamMembers.reduce((acc, curr) => acc + curr.yrsExperience, 0);
+console.log(totalExperience);
 
 // Grouping by a property, and totaling it too
+
